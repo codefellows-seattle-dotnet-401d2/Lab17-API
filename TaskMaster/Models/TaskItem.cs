96ea6922441558c83b6ac35c46bd101d35f1a106
Dustin Mundy <Dustin.A.Mundy@gmail.com>
2018-01-17 20:47:33 -0800

    Initial commit, throwing together some code.

---
+++ TaskMaster/Models/TaskItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskMaster.Models
+{
+    public class TaskItem
+    {
+        //Timestamp when the TaskItem is created
+        public int Created { get; }
+
+        //Optional "Must be complete by" timestamp
+        public int DueBy { get; set; }
+
+        //Optional time to remind the user
+        public int RemindAt { get; set; }
+
+        //Required description
+        public string Description { get; set; }
+
+
+        public TaskItem(String Description)
+        {
+
+        }
+    }
+}
