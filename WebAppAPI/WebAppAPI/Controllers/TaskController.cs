0e1b37051b7f1a9c1fd4b794b2dfb88ec83833f1
Ariel R. Pedraza <pedrazaa2@gmail.com>
2018-01-19 22:10:21 -0800

    controller routes written, not sure it works, testing with postman

--- WebAppAPI/WebAppAPI/Controllers/TaskController.cs
+++ WebAppAPI/WebAppAPI/Controllers/TaskController.cs
@@ -8,6 +8,7 @@ using WebAppAPI.Data;
 
 namespace WebAppAPI.Controllers
 {
+    [Route("api/[controller]")]
     public class TaskController : Controller
     {
         private readonly TaskDbContext _context;
@@ -16,11 +17,44 @@ namespace WebAppAPI.Controllers
         {
             _context = context;
         }
-        
-        // GET: /<controller>/
-        public IActionResult Index()
+
+        // GET: api/<controller>/
+        [HttpGet]
+        public IEnumerable<Tasks> Get()
         {
-            return View();
+            return _context.AllTasks;
+        }
+
+        // GET: api/<controller>/{id}
+        [HttpGet("{id:int}")]
+        public Tasks Get(int id)
+        {
+            return _context.AllTasks.FirstOrDefault(x => x.Id == id);
+        }
+
+        // POST: api/<controller>
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]Tasks value)
+        {
+            await _context.AllTasks.AddAsync(value);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("Get", new { value.Id }, value);
+        }
+
+        // PUT: api/<controller>/{id}
+        [HttpPut("{id:int}")]
+        public void Put(int id, [FromBody]string value)
+        {
+            _context.Update(value);
+            _context.SaveChanges();
+        }
+
+        // DELETE: api/<controller>/{id}
+        [HttpDelete("{id:int}")]
+        public void Delete(int id)
+        {
+            _context.Remove(_context.AllTasks.FirstOrDefault(x => x.Id == id));
+            _context.SaveChanges();
         }
     }
 }
