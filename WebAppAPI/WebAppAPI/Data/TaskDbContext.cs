3f3ee4bbad3ec89c1971c3050302aca2d2b193a5
Ariel R. Pedraza <pedrazaa2@gmail.com>
2018-01-29 14:53:42 -0800

    cache removed

---
+++ WebAppAPI/WebAppAPI/Data/TaskDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAppAPI.Models;
+
+namespace WebAppAPI.Data
+{
+    public class TaskDbContext : DbContext
+    {
+        public TaskDbContext(DbContextOptions<TaskDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Tasks> AllTasks { get; set; }
+    }
+}
